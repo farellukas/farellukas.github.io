b87228f93ff7b95222cca6c5a541f44e421a3121
Farel Lukas <farellukas@gmail.com>
2023-01-06 16:10:10 -0700

    Added about section

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-  import { Router, Route } from "svelte-routing";
+  import About from "./lib/About.svelte";
   import Navbar from "./lib/Navbar.svelte";
-  import About from "./routes/About.svelte";
-  import Contact from "./routes/Contact.svelte";
-  import Projects from "./routes/Projects.svelte";
 </script>
 
-<Router>
+<div class="max-w-4xl m-auto">
   <Navbar />
-  <Route path="/">
-    <About />
-  </Route>
-  <Route path="/projects">
-    <Projects />
-  </Route>
-  <Route path="/contact">
-    <Contact />
-  </Route>
-</Router>
+  <About />
+</div>
